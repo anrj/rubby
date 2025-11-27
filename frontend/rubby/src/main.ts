10aec92770907d536a5cf8e3188075c8c5e5c1c6
Anri Javakhishvili <Javakhishvili.Anri@kiu.edu.ge>
2025-11-27 22:30:01 +0400

    chore: remove placeholder comments in Rubby frontend

--- frontend/rubby/src/main.ts
+++ frontend/rubby/src/main.ts
@@ -7,10 +7,8 @@ if (!duck) {
 } else {
   console.log("Duck element found, setting up simple drag behavior");
 
-  // Add the drag region attribute to the duck element
   duck.setAttribute("data-tauri-drag-region", "");
 
-  // Click detection for chat interface (if needed in future)
   duck.addEventListener("click", () => {
     console.log("Duck clicked!");
     // TODO: Open chat interface when clicked
